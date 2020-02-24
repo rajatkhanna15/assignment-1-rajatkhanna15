016ea615dc54a7b3fd3b99d2cd85ec6c0b3a7b9a
rajatkhanna15 <rajat.khanna_cs18@gla.ac.in>
2020-02-24 22:33:03 +0530

    Main class completed

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
         MyContacts user = new MyContacts();
         boolean response = true;
         do {
-            System.out.println("Welcome to Saurabh's Contact List App\n" +
+            System.out.println("Welcome to Rajat's Contact List App\n" +
                     "Press 1 to add a new contact\n" +
                     "Press 2 to view all contacts\n" +
                     "Press 3 to search for a contact\n" +
